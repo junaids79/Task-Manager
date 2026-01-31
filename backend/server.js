f23ab440fd22f632525c63dbb2f815cd7778fafd
Junaid Shareef <junaids5954@gmail.com>
2026-01-31 19:17:56 +0530

    update html, css

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
-connectDB(); // 🔥 THIS MUST EXIST
+connectDB(); 
 
 const PORT = process.env.PORT || 5000;
 
