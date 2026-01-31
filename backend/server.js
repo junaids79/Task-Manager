e40157eb92c01761ba647fcd148244084fcf0549
Junaid Shareef <junaids5954@gmail.com>
2026-01-31 18:23:38 +0530

    Fix API routing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
+const connectDB = require("./config/db");
 require("dotenv").config();
+
+connectDB(); // 🔥 THIS MUST EXIST
 
 const PORT = process.env.PORT || 5000;
 
