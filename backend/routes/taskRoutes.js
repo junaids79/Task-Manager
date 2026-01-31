05d3d07470cf639b5a363c49d78c608697a40619
Junaid Shareef <junaids5954@gmail.com>
2026-01-31 18:07:13 +0530

    Fix

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskController");
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask
+} = require("../controllers/taskController");
 
 router.get("/", getTasks);
 router.post("/", createTask);
