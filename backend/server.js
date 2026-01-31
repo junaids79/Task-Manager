6ac5e5146afc54ea8634597c52678cae45c92860
Junaid Shareef <junaids5954@gmail.com>
2026-01-31 17:49:29 +0530

    Backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const app = require("./app");
 require("dotenv").config();
-const connectDB = require("./config/db");
-connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  
 });
